3d3cae0a669b44e25cdfaa9205574f5d5ef9fb15
VictorBelotto <victortucunduva.vb@outlook.com>
2023-10-21 18:04:49 -0300

    correção da classe
    Criado o construtor do paper e adicionado a função de buscar os posts pelos IDs.

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,23 +1,59 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
+namespace Chuva\Php\WebScrapping\Entity;
+use DOMDocument;
+use DOMXPath;
+require '../../vendor/autoload.php';
+libxml_use_internal_errors(true);
 
-/**
- * Runner for the Webscrapping exercice.
- */
 class Main {
+  public static function run() {
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+      $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+      $dom = new DOMDocument();
+      $dom->loadHTML($html);
+      $xpath = new DOMXPath($dom);
+      
+      $cardPosts = $xpath->query("//a[@class='paper-card p-lg bd-gradient-left']");
+    
+      $postQuantity = count($cardPosts);
+    
+      $arrayClass = [];
+      foreach ($cardPosts as $cardpost) {
+        $postId = $xpath->query(".//div[@class='volume-info']", $cardpost)->item(0);
+        $postTitle = $xpath->query(".//h4", $cardpost)->item(0);
+        $postType = $xpath->query(".//div[@class='tags mr-sm']", $cardpost)->item(0);
+        $postAuthors = $xpath->query(".//span", $cardpost);
+        $authorsInstituitions = $xpath->query(".//div[@class='authors']/span/@title", $cardpost);
+        
+        $post = new Paper($postId,$postTitle,$postType,$postAuthors);
+        $arrayClass[] = $post;
+      };    
+      return $arrayClass;
+  }  
+};
+    
+function buscaPostId() {
+  $arrayId = func_get_args();
+  $posts = Main::run();
+  print_r( $arrayId);
+  
+  foreach ($posts as $post) {
+    foreach ($arrayId as $idPost) {
+      if ($post->id->textContent === $idPost) {
+        print_r($post);
+      }
+    }
+  }
+}
+/*     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
     print_r($data);
-  }
+  
+ */
 
-}
+
+print_r(buscaPostId('137459', '137460'));
