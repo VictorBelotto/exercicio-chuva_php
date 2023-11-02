16629a810f52136c26a48fbc9d96002012fd9316
VictorBelotto <victortucunduva.vb@outlook.com>
2023-11-02 16:11:20 -0300

    Padrao de comentario

--- php/src/WebScrapping/ExcelGenerator.php
+++ php/src/WebScrapping/ExcelGenerator.php
@@ -8,63 +8,63 @@ use Box\Spout\Common\Entity\Style\Color;
 
 class ExcelGenerator {
 
-    static function createStyleHeader() {
-        $style = (new StyleBuilder())
-            ->setFontBold()
-            ->setFontSize(15)
-            ->setBackgroundColor(Color::LIGHT_GREEN)
-            ->build();
-        return $style;
+  static function createStyleHeader() {
+    $style = (new StyleBuilder())
+      ->setFontBold()
+      ->setFontSize(15)
+      ->setBackgroundColor(Color::LIGHT_GREEN)
+      ->build();
+    return $style;
+  }
+
+  static function headerSpreadsheet() {
+    $authorsAndInstitutionArray = [];
+    $quantAuthor = 16;
+    for ($i = 1; $i <= $quantAuthor; $i++) {
+        $authorName = "Author $i";
+        $authorInstitution = "Author $i Institution";
+
+        $authorsAndInstitutionArray[] = $authorName;
+        $authorsAndInstitutionArray[] = $authorInstitution;
     }
+    $headerSpreadsheet = ["ID", "Title", "Type"];
+    $rowHeaderSpreadsheet = array_merge($headerSpreadsheet, $authorsAndInstitutionArray);
+    return $rowHeaderSpreadsheet;
+  }
 
-    static function headerSpreadsheet() {
-        $authorsAndInstitutionArray = [];
-        $quantAuthor = 16;
-        for ($i = 1; $i <= $quantAuthor; $i++) {
-            $authorName = "Author $i";
-            $authorInstitution = "Author $i Institution";
+  function generate($data) : void {
+      $data = $data;
+      $filePath = __DIR__ . '/../../assets/resultadoWebscraping.xlsx';
+      $writer = WriterEntityFactory::createXLSXWriter();
+      $writer->openToFile($filePath);
 
-            $authorsAndInstitutionArray[] = $authorName;
-            $authorsAndInstitutionArray[] = $authorInstitution;
+      function createRowPostsInfos($data, $writer) {
+        for ($i = 0; $i < count($data); $i++){
+          $id = $data[$i]->id;
+          $title = $data[$i]->title;
+          $type = $data[$i]->type;
+          $arrayRow = [$id, $title, $type];
+
+          $authorName = $data[$i]->authors->name;
+          $authorInstitution = $data[$i]->authors->institution;
+          $arrayAuthorsAndInstitution = [];
+          
+          for($a = 0; $a < count($authorName); $a++){
+              $arrayAuthorsAndInstitution[] = $authorName[$a];
+              $arrayAuthorsAndInstitution[] = $authorInstitution[$a];
+          }
+
+          $linhaAuthors = $arrayAuthorsAndInstitution;
+          $merged = array_merge($arrayRow, $linhaAuthors);
+          $linha = WriterEntityFactory::createRowFromArray($merged);
+          $writer->addRow($linha); 
         }
-        $headerSpreadsheet = ["ID", "Title", "Type"];
-        $rowHeaderSpreadsheet = array_merge($headerSpreadsheet, $authorsAndInstitutionArray);
-        return $rowHeaderSpreadsheet;
-    }
+      }
 
-    function generate($data) : void {
-        $data = $data;
-        $filePath = __DIR__ . '/../../assets/resultadoWebscraping.xlsx';
-        $writer = WriterEntityFactory::createXLSXWriter();
-        $writer->openToFile($filePath);
-
-        function createRowPostsInfos($data, $writer) {
-            for ($i = 0; $i < count($data); $i++){
-                $id = $data[$i]->id;
-                $title = $data[$i]->title;
-                $type = $data[$i]->type;
-                $arrayRow = [$id, $title, $type];
-
-                $authorName = $data[$i]->authors->name;
-                $authorInstitution = $data[$i]->authors->institution;
-                $arrayAuthorsAndInstitution = [];
-                
-                for($a = 0; $a < count($authorName); $a++){
-                    $arrayAuthorsAndInstitution[] = $authorName[$a];
-                    $arrayAuthorsAndInstitution[] = $authorInstitution[$a];
-                }
-
-                $linhaAuthors = $arrayAuthorsAndInstitution;
-                $merged = array_merge($arrayRow, $linhaAuthors);
-                $linha = WriterEntityFactory::createRowFromArray($merged);
-                $writer->addRow($linha); 
-            }
-        }
-  
-        $criaHeader = WriterEntityFactory::createRowFromArray($this->headerSpreadsheet(), $this::createStyleHeader());
-        $writer->addRow($criaHeader);
-        createRowPostsInfos($data, $writer);
-        $writer->close(); 
-        echo 'Vamos Chover!';
-    }
+      $criaHeader = WriterEntityFactory::createRowFromArray($this->headerSpreadsheet(), $this::createStyleHeader());
+      $writer->addRow($criaHeader);
+      createRowPostsInfos($data, $writer);
+      $writer->close(); 
+      echo 'Vamos Chover!';
+  }
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -5,33 +5,40 @@ namespace Chuva\Php\WebScrapping;
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
 
+/**
+ * Does the scrapping of a webpage.
+ */
 class Scrapper {
-    public function scrap($dom): array {
-        $xpath = new \DOMXPath($dom);
-        $cardPosts = $xpath->query("//a[@class='paper-card p-lg bd-gradient-left']");
-        $arrayPapers = [];
 
-        foreach ($cardPosts as $cardpost) {
-            $postId = $xpath->query(".//div[@class='volume-info']", $cardpost)->item(0)->textContent;
-            $postTitle = $xpath->query(".//h4", $cardpost)->item(0)->textContent;
-            $postType = $xpath->query(".//div[@class='tags mr-sm']", $cardpost)->item(0)->textContent;
-            $postAuthors = $xpath->query(".//span", $cardpost);
-            $authorsInstituitions = $xpath->query(".//div[@class='authors']/span/@title", $cardpost);
+  /**
+   * Loads paper information from the HTML and returns the array with the data.
+   */
+  public function scrap($dom): array {
+      $xpath = new \DOMXPath($dom);
+      $cardPosts = $xpath->query("//a[@class='paper-card p-lg bd-gradient-left']");
+      $arrayPapers = [];
 
-            $postAuthorsArray = [];
-            foreach ($postAuthors as $author) {
-                $postAuthorsArray[] = $author->textContent;
-            }
-            $postInstituitionsArray = [];
-            foreach ($authorsInstituitions as $institution) {
-                $postInstituitionsArray[] = $institution->textContent;
-            }
+      foreach ($cardPosts as $cardpost) {
+          $postId = $xpath->query(".//div[@class='volume-info']", $cardpost)->item(0)->textContent;
+          $postTitle = $xpath->query(".//h4", $cardpost)->item(0)->textContent;
+          $postType = $xpath->query(".//div[@class='tags mr-sm']", $cardpost)->item(0)->textContent;
+          $postAuthors = $xpath->query(".//span", $cardpost);
+          $authorsInstituitions = $xpath->query(".//div[@class='authors']/span/@title", $cardpost);
 
-            $authors = new Person($postAuthorsArray, $postInstituitionsArray);
-            $papers = new Paper($postId, $postTitle, $postType, $authors);
-            $arrayPapers[] = $papers;
-        }
+          $postAuthorsArray = [];
+          foreach ($postAuthors as $author) {
+              $postAuthorsArray[] = $author->textContent;
+          }
+          $postInstituitionsArray = [];
+          foreach ($authorsInstituitions as $institution) {
+              $postInstituitionsArray[] = $institution->textContent;
+          }
 
-        return $arrayPapers;
-    }
+          $authors = new Person($postAuthorsArray, $postInstituitionsArray);
+          $papers = new Paper($postId, $postTitle, $postType, $authors);
+          $arrayPapers[] = $papers;
+      }
+
+      return $arrayPapers;
+  }
 }
