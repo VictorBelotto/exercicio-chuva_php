ce4ea3f55b9ce2d46d87586e6588eb6ae0b456e6
VictorBelotto <victortucunduva.vb@outlook.com>
2023-11-02 11:15:22 -0300

    IDENTAÇÃO

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -4,18 +4,17 @@ namespace Chuva\Php\WebScrapping;
 use DOMDocument;
 libxml_use_internal_errors(true);
 class Main {
-  public static function run(): void
-    {
-      $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
-      $dom = new \DOMDocument();
-      $dom->loadHTML($html);
+  public static function run(): void {
+  $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
+  $dom = new \DOMDocument();
+  $dom->loadHTML($html);
 
-      $scrapper = new Scrapper();
-      $data = $scrapper->scrap($dom);
+  $scrapper = new Scrapper();
+  $data = $scrapper->scrap($dom);
 
-      $generator = new ExcelGenerator();
-      $generator->generate($data);
-    }
+  $generator = new ExcelGenerator();
+  $generator->generate($data);
+  }
 
 };
   
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -7,43 +7,43 @@ use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
 libxml_use_internal_errors(true);
 
- class Scrapper {
-  /*Função principal onde é feito toda a raspagem dos dados da origem.html */
+class Scrapper {
+/*Função principal onde é feito toda a raspagem dos dados da origem.html */
   
   public function scrap($dom): array {
    
-    $xpath = new \DOMXPath($dom);
+  $xpath = new \DOMXPath($dom);
+  
+  $cardPosts = $xpath->query("//a[@class='paper-card p-lg bd-gradient-left']");
+  $arrayPapers = [];
+  $arrayPersons = [];
+
+  foreach ($cardPosts as $cardpost) {
+  $postId = $xpath->query(".//div[@class='volume-info']", $cardpost)->item(0)->textContent;
+  $postTitle = $xpath->query(".//h4", $cardpost)->item(0)->textContent;
+  $postType = $xpath->query(".//div[@class='tags mr-sm']", $cardpost)->item(0)->textContent;
+  $postAuthors = $xpath->query(".//span", $cardpost);
+  $authorsInstituitions = $xpath->query(".//div[@class='authors']/span/@title", $cardpost);
     
-    $cardPosts = $xpath->query("//a[@class='paper-card p-lg bd-gradient-left']");
-    $arrayPapers = [];
-    $arrayPersons = [];
-
-    foreach ($cardPosts as $cardpost) {
-      $postId = $xpath->query(".//div[@class='volume-info']", $cardpost)->item(0)->textContent;
-      $postTitle = $xpath->query(".//h4", $cardpost)->item(0)->textContent;
-      $postType = $xpath->query(".//div[@class='tags mr-sm']", $cardpost)->item(0)->textContent;
-      $postAuthors = $xpath->query(".//span", $cardpost);
-      $authorsInstituitions = $xpath->query(".//div[@class='authors']/span/@title", $cardpost);
+  /*ambos os foreachs guardam o text content dentro de um array e assim separa entre os 62 posts (62 indices no array), sendo cada indice todos os autores e instituições de um post */
       
-      /*ambos os foreachs guardam o text content dentro de um array e assim separa entre os 62 posts (62 indices no array), sendo cada indice todos os autores e instituições de um post */
-      
-      $postAuthorsArray = [];
-      foreach ($postAuthors as $author) {
-          $postAuthorsArray[] = $author->textContent;
-      }
-      $postInstituitionsArray = [];
-      foreach ($authorsInstituitions as $institution) {
-          $postInstituitionsArray[] = $institution->textContent;
-      }
-
-      /*a estrutura da classe Person foi modificada para receber um array de autores e outro de instituiçoes, pois ja tinha criado um metodo que iria trabalhar com a ordem deles */
-      $papers = new Paper($postId,$postTitle,$postType);
-      $persons = new Person($postAuthorsArray, $postInstituitionsArray);
-      $arrayPapers[] = $papers;
-      $arrayPersons[] = $persons;
-    };    
-    /*aqui é onde retorna os dados para tratar dentro do excel generator */
-    return [$arrayPapers, $arrayPersons];
+  $postAuthorsArray = [];
+  foreach ($postAuthors as $author) {
+    $postAuthorsArray[] = $author->textContent;
+  }
+  $postInstituitionsArray = [];
+  foreach ($authorsInstituitions as $institution) {
+    $postInstituitionsArray[] = $institution->textContent;
   }
 
- }
+  /*a estrutura da classe Person foi modificada para receber um array de autores e outro de instituiçoes, pois ja tinha criado um metodo que iria trabalhar com a ordem deles */
+  $papers = new Paper($postId,$postTitle,$postType);
+  $persons = new Person($postAuthorsArray, $postInstituitionsArray);
+  $arrayPapers[] = $papers;
+  $arrayPersons[] = $persons;
+  };    
+    /*aqui é onde retorna os dados para tratar dentro do excel generator */
+  return [$arrayPapers, $arrayPersons];
+}
+
+}
