8abda9c3b97ea5e02f128d08961ca12cb8271fb4
VictorBelotto <victortucunduva.vb@outlook.com>
2023-11-02 14:28:53 -0300

    Tentativa de Correção arquivo ExcelGenerate
    Tentativa de sumir os erros de identação

--- php/src/WebScrapping/ExcelGenerator.php
+++ php/src/WebScrapping/ExcelGenerator.php
@@ -23,52 +23,51 @@ class ExcelGenerator {
         $quantAuthor = 16;
         for ($i = 1; $i <= $quantAuthor; $i++) {
             $authorName = "Author $i";
-            $authorInstituition = "Author $i Instituition";
+            $authorInstitution = "Author $i Institution";
 
             $authorsAndInstitutionArray[] = $authorName;
-            $authorsAndInstitutionArray[] = $authorInstituition;
+            $authorsAndInstitutionArray[] = $authorInstitution;
         }
         $headerSpreadsheet = ["ID", "Title", "Type"];
         /* Retorna a linha do cabeçalho formatada */
-        return $rowHeaderSpreadsheet = array_merge($headerSpreadsheet, $authorsAndInstitutionArray);
+        $rowHeaderSpreadsheet = array_merge($headerSpreadsheet, $authorsAndInstitutionArray);
+        return $rowHeaderSpreadsheet;
     }
 
     function generate($data) : void {
         /* Data é um array que contém 2 dados: Um array de objetos da classe Paper e um array que armazena Person */
         $data = $data;
-        $filePath = __DIR__ . '/../../assets/resultadoWebscrappring.xlsx';
+        $filePath = __DIR__ . '/../../assets/resultadoWebscraping.xlsx';
         $writer = WriterEntityFactory::createXLSXWriter();
         $writer->openToFile($filePath);
-        /* Essa função alem de criar as linhas da tabela por um array, também ordena o array de em sequência de autor1 e instituição1 e assim por diante */
+
+        /* Esta função alem de criar as linhas da tabela por um array, também ordena o array em sequência de autor1 e instituição1 e assim por diante */
         function createRowPostsInfos($data, $writer) {
-
-          for ($i = 0; $i < count($data); $i++){
-            $id = $data[$i]->id;
-            $title = $data[$i]->title;
-            $type = $data[$i]->type;
-            $arrayRow = [$id, $title, $type];
-
+            for ($i = 0; $i < count($data); $i++){
+                $id = $data[$i]->id;
+                $title = $data[$i]->title;
+                $type = $data[$i]->type;
+                $arrayRow = [$id, $title, $type];
 
                 $authorName = $data[$i]->authors->name;
-                $authorInstituition =  $data[$i]->authors->institution;
-                $arrayAuthorsAndInstituition = [];
+                $authorInstitution = $data[$i]->authors->institution;
+                $arrayAuthorsAndInstitution = [];
+                
                 /* Este loop recebe a quantidade de autores relativos à posição acima para realizar a ordem de autor 1 e instituição 1.... */
                 for($a = 0; $a < count($authorName); $a++){
-                    $arrayAuthorsAndInstituition[] = $authorName[$a];
-                    $arrayAuthorsAndInstituition[] = $authorInstituition[$a];
+                    $arrayAuthorsAndInstitution[] = $authorName[$a];
+                    $arrayAuthorsAndInstitution[] = $authorInstitution[$a];
                 }
-                /* Para cada posição do loop "objectPersons", faremos a criação da linha da planilha formatada com os dados em ordem */  
-                
-                $linhaAuthors = $arrayAuthorsAndInstituition;
-                $merged =  array_merge($arrayRow, $linhaAuthors );
-                $linha =  WriterEntityFactory::createRowFromArray($merged) ;
-                $writer->addRow($linha); 
-          }
 
-            
+                /* Para cada posição do loop "objectPersons", faremos a criação da linha da planilha formatada com os dados em ordem */  
+                $linhaAuthors = $arrayAuthorsAndInstitution;
+                $merged = array_merge($arrayRow, $linhaAuthors);
+                $linha = WriterEntityFactory::createRowFromArray($merged);
+                $writer->addRow($linha); 
+            }
         }
   
-        $criaHeader = WriterEntityFactory::createRowFromArray($this->headerSpreadsheet(), $this->createStyleHeader()) ; 
+        $criaHeader = WriterEntityFactory::createRowFromArray($this->headerSpreadsheet(), $this::createStyleHeader());
         $writer->addRow($criaHeader);
         createRowPostsInfos($data, $writer);
         $writer->close(); 
