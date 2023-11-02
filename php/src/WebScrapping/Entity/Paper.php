e421390d5569f358b162a7ca5867f8c9ba15d4a6
VictorBelotto <victortucunduva.vb@outlook.com>
2023-11-02 14:56:28 -0300

    classe paper

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -3,31 +3,38 @@
 namespace Chuva\Php\WebScrapping\Entity;
 
 class Paper {
-  /**
-   * Paper Id.
-   *
-   * @var int
-   */
-  public $id;
-  /**
-   * Paper Title.
-   *
-   * @var string
-   */
-  public $title;
-  /**
-   * The paper type (e.g. Poster, Nobel Prize, etc).
-   *
-   * @var string
-   */
-  public  $type;
-
-  public $authors;
-  
-  public function __construct($id, $title, $type, $authors ) {
-    $this->id = $id;
-    $this->title = $title;
-    $this->type = $type;
-    $this->authors = $authors;
-  }
+    /**
+     * Paper Id.
+     *
+     * @var int
+     */
+    public $id;
+    
+    /**
+     * Paper Title.
+     *
+     * @var string
+     */
+    public $title;
+    
+    /**
+     * The paper type (e.g. Poster, Nobel Prize, etc).
+     *
+     * @var string
+     */
+    public $type;
+    
+    /**
+     * Paper Authors.
+     *
+     * @var object
+     */
+    public $authors;
+    
+    public function __construct($id, $title, $type, $authors) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->type = $type;
+        $this->authors = $authors;
+    }
 }
