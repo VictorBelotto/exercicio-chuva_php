7b7e5f7f072e4b6c8f52fc6d5e08c6f56538a15b
VictorBelotto <victortucunduva.vb@outlook.com>
2023-11-02 16:51:44 -0300

    correção paper

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -5,44 +5,44 @@ namespace Chuva\Php\WebScrapping\Entity;
 /**
  * The Paper class represents the row of the parsed data.
  */
-class Paper
-{
-    /**
-     * Paper Id.
-     *
-     * @var int
-     */
-    public $id;
-  
-    /**
-     * Paper Title.
-     *
-     * @var string
-     */
-    public $title;
-  
-    /**
-     * The paper type (e.g. Poster, Nobel Prize, etc).
-     *
-     * @var string
-     */
-    public $type;
-  
-    /**
-     * Paper Authors.
-     *
-     * @var object
-     */
-    public $authors;
-  
-    /**
-     * Builder.
-     */
-    public function __construct($id, $title, $type, $authors)
-    {
-        $this->id = $id;
-        $this->title = $title;
-        $this->type = $type;
-        $this->authors = $authors;
-    }
+class Paper{
+
+  /**
+   * Paper Id.
+   *
+   * @var int
+   */
+  public $id;
+
+  /**
+   * Paper Title.
+   *
+   * @var string
+   */
+  public $title;
+
+  /**
+   * The paper type (e.g. Poster, Nobel Prize, etc).
+   *
+   * @var string
+   */
+  public $type;
+
+  /**
+   * Paper Authors.
+   *
+   * @var object
+   */
+  public $authors;
+
+  /**
+   * Builder.
+   */
+  public function __construct($id, $title, $type, $authors){
+    $this->id = $id;
+    $this->title = $title;
+    $this->type = $type;
+    $this->authors = $authors;
+  }
+
 }
